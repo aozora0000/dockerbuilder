ecd8f1ee3f221d36d7a82764c4d84eb9d461cc2c
kohei kinoshita <aozora0000@gmail.com>
2015-01-16 13:49:28 +0900

    first commit

---
+++ src/Dockerbuilder/Command/BuildCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dockerbuilder\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BuildCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('build')
+            ->setDescription('docker build each git branch');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        exec('git rev-parse --branches');
+        $output->writeln('Hello World');
+    }
+    /*
+    git rev-parse --branches | while read BRANCH_HASH; do \
+    echo $(git branch --contains=$BRANCH_HASH) \
+    done
+     */
+}
