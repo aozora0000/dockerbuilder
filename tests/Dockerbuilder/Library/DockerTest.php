8126ee9bfe187198a772cf8548e4f6f6c0bd363c
kohei kinoshita <aozora0000@gmail.com>
2015-02-07 14:19:18 +0900

    delete test Dockertest

--- tests/Dockerbuilder/Library/DockerTest.php
+++ tests/Dockerbuilder/Library/DockerTest.php
@@ -6,15 +6,4 @@ class DockerTest extends PHPUnit_Framework_TestCase {
     public function testAvailable() {
         $this->assertFalse(Docker::available());
     }
-
-    public function testBuild() {
-        $result = Docker::build("dockerbuilder","dev");
-        $this->assertEquals(1,$result->exitcode);
-    }
-
-    public function testClean() {
-        Docker::clean();
-        $result = Docker::clean();
-        $this->assertEquals(2,$result->exitcode);
-    }
 }
